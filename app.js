4857aacac02b624a63e098996fb2bc32b62a7a29
AlexAlonRo <ar2782819@gmail.com>
2022-02-07 14:12:22 -0600

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -26,8 +26,11 @@ app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://ale:1234@cluster0.9fehp.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://ale:1234@cluster0.9fehp.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
